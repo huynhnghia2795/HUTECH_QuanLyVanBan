dbfbadacff0fff659251b8c68f53c00d98a5dc9a
Kevin Pham <huynhnghia2795@gmail.com>
2016-11-16 22:47:36 +0700

    Create Mvc 5 Project & Delete Danh Muc
    Tạo project Mvc 5 và thiết lập chức năng delete danh mục

---
+++ WebQLVanBan/Controllers/QLVanBanController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebQLVanBan.Models;
+
+namespace WebQLVanBan.Controllers
+{
+    public class QLVanBanController : Controller
+    {
+        // GET: QLVanBan
+        DataClassesQLVanBanDataContext db = new DataClassesQLVanBanDataContext();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Danhmuc()
+        {
+            return View(db.DanhMucs.ToList());
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            DanhMuc g = db.DanhMucs.SingleOrDefault(n => n.DanhMucID == id);
+            ViewBag.DanhMucID = g.DanhMucID;
+            if (g == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(g);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Xacnhanxoa(int id)
+        {
+            DanhMuc g = db.DanhMucs.SingleOrDefault(n => n.DanhMucID == id);
+            ViewBag.DanhMucID = g.DanhMucID;
+            if (g == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.DanhMucs.DeleteOnSubmit(g);
+            db.SubmitChanges();
+            return RedirectToAction("Danhmuc");
+
+        }
+    }
+}
